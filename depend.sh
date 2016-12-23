d364aec22cff543712ef5477372de6514586ebd0
zhangzheng <18601254750.163.com>
2016-12-23 11:48:16 +0800

    安装脚本增加权限检查

--- depend.sh
+++ depend.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+role=`id -u`
+if test $role -ne 0
+then
+    echo "运行脚本需要root权限"
+    exit 1
+fi
+
 yum install python-setuptools.noarch
+
+command -v wget || yum -y install wget || exit 1
+
 easy_install pip
 pip install pymongo
 pip install beautifulsoup4
