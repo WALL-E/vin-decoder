f23cc79ca4b857a6b64abac9117dd24c83dc3c77
zhangzheng <zhangzheng@qianbao.com>
2016-12-23 20:23:48 +0800

    Restful brower of VinDecoder

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,8 +1,18 @@
+var mainApp = angular.module("mainApp", ['ngPrettyJson']);
 
-function vinSearchCtrl($scope) {
-    $scope.vinCode = "";
+mainApp.controller("vinSearchController", function($scope, $http) {
+    var baseUrl = "http://192.168.1.231:10089/vin/v1/";
+    $scope.vinCode = "LVSHCAMB1CE054249";
+    $scope.result = {};
 
     $scope.search = function() {
-        $scope.result = "Ford";
+        $http({
+            url: baseUrl + $scope.vinCode,
+            method:'GET'
+        }).success(function(data,header,config,status){
+            $scope.result = data;
+        }).error(function(data,header,config,status){
+            console.log("search VinCode failed");
+        });
     }
-}
+});
