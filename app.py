8ba76ef206521f122abb773812860eeae10ddd22
zhangzheng <zhangzheng@qianbao.com>
2016-12-24 07:04:46 +0800

    使用vinObj.getVin获取vincode

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ class VinCodeHandler(tornado.web.RequestHandler):
                 "status": "40400000", 
                 "message": "not found",
             }
-            RabbitMQ().publish(vincode)
+            RabbitMQ().publish(vinObj.getVin())
             self.write(json.dumps(res, ensure_ascii=False))
         else:
             result.pop("_id")
