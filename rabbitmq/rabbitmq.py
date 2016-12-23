56cdb68463949b499c03e855d7c9ffbd13c77c8c
zhangzheng <zhangzheng@qianbao.com>
2016-12-24 05:52:56 +0800

    封装rambbitmq对象

---
+++ rabbitmq/rabbitmq.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pika
+
+class RabbitMQ():
+    def __init__(self, host="localhost", port=5672, queue="vin"):
+        self.host = host
+        self.port = port
+        self.queue = queue
+
+    def publish(self, content):
+        credentials = pika.PlainCredentials('guest', 'guest')
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.host, self.port, '/', credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue)
+        channel.basic_publish(exchange='', routing_key=self.queue, body=content)
+        connection.close()
+
+    def start_consuming(self, callback):
+        credentials = pika.PlainCredentials('guest', 'guest')
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.host, self.port, '/', credentials))
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue)
+        channel.basic_consume(callback, queue=self.queue, no_ack=True)
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+if __name__ == '__main__':
+    import time
+    mq = RabbitMQ(queue="test")
+    for i in range(5):
+        mq.publish("hello, world, @%f" % (time.time()))
+    mq.start_consuming(callback)
