12311039fe7192272e58e1e6229d3ab15acb1818
zhangzheng <zhangzheng@qianbao.com>
2016-12-23 18:54:13 +0800

    完成html数据的解析

--- html/t.py
+++ html/t.py
@@ -1,11 +1,39 @@
 #!/usr/bin/python
+# coding:utf-8
+#
+# 从Html中解析数据
+#
 
 
 from bs4 import BeautifulSoup
+import json
 
-soup = BeautifulSoup(open('LVSHCAMB1CE054249.html'), "html.parser")
+def parse_vin114_net():
+    soup = BeautifulSoup(open('LVSHCAMB1CE054249.html'), "html.parser")
 
-print soup.table.prettify()
+    tables = soup.findAll('table')
+    table = tables[0]
 
-#for child in soup.table.children:
-#    print child
+    result_list = []
+    for row in table.findAll('tr'):
+        for child in row.children:
+            ret = child.string.strip()
+            if len(ret) > 0:
+                result_list.append(ret.encode("utf8"))
+
+    result_dict = {}
+    for i in range(0, len(result_list), 2):
+        k = result_list[i]
+        v = result_list[i+1]
+        result_dict[k] = v
+
+    return json.dumps(result_dict, encoding='UTF-8', ensure_ascii=False)
+
+
+def main():
+    print parse_vin114_net()
+
+
+if __name__ == "__main__":
+    main()
+
