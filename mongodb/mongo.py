08e2b5d33f72de934a2f5382d343c91bf3836748
zhangzheng <zhangzheng@qianbao.com>
2016-12-24 03:29:29 +0800

    增加mongodb DAO

---
+++ mongodb/mongo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from pymongo import *
+import json
+
+
+class Mongo():
+    def __init__(self, host="localhost", port=27017):
+        self.host = host
+        self.port = port
+
+    def query_vin(self, vin_code):
+        client = MongoClient() 
+        client = MongoClient(self.host, self.port)
+        db = client["vin"]
+        collection = db["vin"]
+        result = collection.find_one({"vin":vin_code})
+        return result
+
+    def query_wmi(self, wmi_code):
+        client = MongoClient() 
+        client = MongoClient(self.host, self.port)
+        db = client["vin"]
+        collection = db["wmi"]
+        result = collection.find_one({"wmi":wmi_code})
+        return result
+
+
+if __name__ == '__main__':
+    mongo = Mongo()
+    result = mongo.query_wmi("LSG")
+    print type(result), result
+    result = mongo.query_vin("LSVAM4187C2184847")
+    print type(result), result
+
