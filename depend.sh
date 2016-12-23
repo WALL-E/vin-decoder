4c6b8506cefcffc2bcc19f9b3d75cdd24210d819
zhangzheng <zhangzheng@qianbao.com>
2016-12-23 19:22:28 +0800

    增加mongodb @depend.sh

--- depend.sh
+++ depend.sh
@@ -7,6 +7,15 @@ then
     exit 1
 fi
 
+cat <<'EOF' > /etc/yum.repos.d/mongodb-org-3.0.repo
+[mongodb-org-3.0]
+name=MongoDB Repository
+baseurl=http://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y mongodb-org
 yum -y install python-setuptools.noarch
 yum -y install python-tornado.noarch
 yum -y install python-requests.noarch
