a0ebb23f33952438aaf44f73127fa46f6666e410
zhangzheng <zhangzheng@qianbao.com>
2016-12-24 09:12:55 +0800

    可以命令行指定队列名称

--- rabbitmq/consume.py
+++ rabbitmq/consume.py
@@ -3,12 +3,16 @@
 
 from rabbitmq import RabbitMQ
 import time
+import sys
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 def main():
-    mq = RabbitMQ()
+    queue = "vin"
+    if len(sys.argv) >= 2:
+        queue = sys.argv[1]
+    mq = RabbitMQ(queue=queue)
     mq.start_consuming(callback)
 
 if __name__ == '__main__':
