a5ada529cffb80d88a9aae22ba36957aa0a05b20
zhangzheng <zhangzheng@qianbao.com>
2016-12-25 03:02:49 +0800

    增加调试地址

--- h5/static/js/app.js
+++ h5/static/js/app.js
@@ -1,7 +1,8 @@
 var mainApp = angular.module("mainApp", ['ngPrettyJson']);
 
 mainApp.controller("vinSearchController", function($scope, $http) {
-    var baseUrl = "http://172.28.32.101:10090/vin/v1/";
+    //var baseUrl = "http://172.28.32.101:10090/vin/v1/";
+    var baseUrl = "http://192.168.1.231:10090/vin/v1/";
     $scope.vinCode = "LVSHCAMB1CE054249";
     $scope.result = {};
     $scope.search = function(vinCode) {
