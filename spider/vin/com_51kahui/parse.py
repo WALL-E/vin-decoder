da8a84a32a3afd4090edd9ebf2743f195a6d671a
zhangzheng <zhangzheng@qianbao.com>
2016-12-25 23:18:44 +0800

    规范化worker

---
+++ spider/vin/com_51kahui/parse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# coding:utf-8
+"""
+从Html文件中解析车辆信息
+"""
+
+import sys
+import os
+import json
+
+from bs4 import BeautifulSoup
+
+
+def parse_html(html):
+    """
+    parse html of https://vincar.cn/
+
+    :param html: html strings
+    :returns: list
+    """
+    results = []
+    try:
+        data = json.loads(html)
+        print data
+        if data["status"] == "20000000":
+            result = data["result"]
+            results.append(result)
+    except Exception, msg:
+        print "error: [parse html] %s" % (msg)
+    return results
+
+
+def main():
+    """
+    main function
+    """
+    if len(sys.argv) < 2:
+        print "%s filename.html" % (sys.argv[0])
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print "%s is not exists" % (filename)
+        sys.exit(1)
+    html = open(filename).readline().strip()
+    data = parse_html(html)
+    print "result:", json.dumps(data, encoding='UTF-8', ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ spider/vin/com_51kahui/robot.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+模拟用户提交请求
+"""
+
+import sys
+import os
+import time
+
+import requests
+
+ROOT_DIR = os.path.dirname(__file__)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, '../../../proxy'))
+
+import proxy
+
+URL = "http://spider.51kahui.com/CarInfo/vinQuery"
+TIMEOUT = 60
+TRY_COUNT = 10
+
+def robot_html(vin_code, proxy_use=True, proxy_reuse=False):
+    """
+    模拟用户提交网页POST请求
+
+    :param vin_code:    string, vin code
+    :returns: string or None
+    """
+    html = None
+    # 1-Request
+    response = None
+    for _ in range(TRY_COUNT):
+        proxies = {}
+        if proxy_use:
+            print "use proxy to post data"
+            server = proxy.next_server()
+            if server is None:
+                print "No agents available, wating 60s"
+                time.sleep(60)
+                continue
+            proxies["http"] = "http://%s" % (server)
+        try:
+            payload = {"vinCode": vin_code}
+            response = requests.post(URL, proxies=proxies, timeout=TIMEOUT, data=payload)
+        except Exception, msg:
+            print msg
+            continue
+
+        print "[1]", response
+        if response is not None and response.status_code == 200:
+            if proxy_use and proxy_reuse:
+                proxy.requeue_server(server)
+            html = response.text
+            break
+
+    if response is None:
+        print "No agents available, retry %s" % (TRY_COUNT)
+        return html
+
+    return html
+
+
+if __name__ == '__main__':
+    print robot_html("LVSHCAMB1CE054249", proxy_use=False, proxy_reuse=False)
