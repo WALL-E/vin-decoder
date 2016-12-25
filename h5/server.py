a78f45e4cd84bf123648ccf8f5ca45c530fe5bf1
zhangzheng <zhangzheng@qianbao.com>
2016-12-25 13:59:58 +0800

    重构server.py

--- h5/server.py
+++ h5/server.py
@@ -4,11 +4,14 @@
 import SimpleHTTPServer
 import SocketServer
 
+IP = "0.0.0.0"
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+def main():
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer((IP, PORT), Handler)
+    print "serving at port", PORT
+    httpd.serve_forever()
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-httpd.serve_forever()
+if __name__ == "__main__":
+    main()
