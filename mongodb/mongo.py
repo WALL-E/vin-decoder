c6585c3bd8ddfb52131478a02677853bd9b1955f
zhangzheng <zhangzheng@qianbao.com>
2016-12-26 07:36:44 +0800

    优化mongo.py

--- mongodb/mongo.py
+++ mongodb/mongo.py
@@ -5,28 +5,24 @@ import json
 
 
 class Mongo():
-    def __init__(self, host="localhost", port=27017):
+    def __init__(self, host="localhost", port=27017, dbname="vin"):
         self.host = host
         self.port = port
+        self.conn = MongoClient(self.host, self.port)
+        self.db = self.conn[dbname]
 
     def query_vin(self, vin_code):
-        client = MongoClient(self.host, self.port)
-        db = client["vin"]
-        collection = db["vin"]
+        collection = self.db["vin"]
         results = collection.find({"vinCode":vin_code})
         return results
 
     def query_wmi(self, wmi_code):
-        client = MongoClient(self.host, self.port)
-        db = client["vin"]
-        collection = db["wmi"]
+        collection = self.db["wmi"]
         results = collection.find({"wmiCode":wmi_code})
         return results
 
     def insert_vin(self, objs, vin_code):
-        client = MongoClient(self.host, self.port)
-        db = client["vin"]
-        collection = db["vin"]
+        collection = self.db["vin"]
         for obj in objs:
             obj["vinCode"] = vin_code[0:8]
             obj["vinLong"] = vin_code
@@ -35,10 +31,10 @@ class Mongo():
 
 if __name__ == '__main__':
     mongo = Mongo()
-    results = mongo.query_vin("WP0AA298")
+    results = mongo.query_vin("LSVAM418")
     for result in results:
         print "vin:", type(result), result
-    results = mongo.query_wmi("LFV")
+    results = mongo.query_wmi("LVG")
     for result in results:
         print "wmi:", type(result), result
 
