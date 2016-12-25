a60426ea40a7f86579e8b184b2706ff6bbf1df25
zhangzheng <zhangzheng@qianbao.com>
2016-12-25 20:49:34 +0800

    规范化 worker

--- spider/vin/net_vin114/worker.py
+++ spider/vin/net_vin114/worker.py
@@ -12,18 +12,14 @@ from pymongo import *
 ROOT_DIR = os.path.dirname(__file__)
 sys.path.append(ROOT_DIR)
 sys.path.append(os.path.join(ROOT_DIR, '../../../rabbitmq'))
+sys.path.append(os.path.join(ROOT_DIR, '../../../mongodb'))
 
 from rabbitmq import RabbitMQ
+from mongo import Mongo
 from robot import robot_html
 from parse import parse_html
 
 timeout = 60
-
-client = MongoClient()
-client = MongoClient("localhost", 27017)
-db = client["vin"]
-collection = db["vin"]
-
 
 def do_task(vin_code):
     print "do_task(): %s" % (vin_code)
@@ -35,20 +31,15 @@ def do_task(vin_code):
         else:
             print "[1] %s not found, parse html failed" % (vin_code)
     else:
-        print "[2] %s not found, download page failed" % (vin_code)
+        print "[2]%s not found, download page failed" % (vin_code)
     return None
 
 
-def insert_db(obj, vin_code):
-    obj["vinCode"] = vin_code[0:8]
-    obj["vinLong"] = vin_code
-    collection.insert(obj)
-
 def do_once(vin_code):
-    result = do_task(vin_code)
-    if result:
-        insert_db(result, vin_code)
-    print "result: %s" % (result)
+    results = do_task(vin_code)
+    if results:
+        Mongo().insert_vin(results, vin_code)
+    print "result: %s" % (results)
 
 def do_loop():
     mq = RabbitMQ(queue="vin")
@@ -56,13 +47,13 @@ def do_loop():
         vin_code = mq.basic_get()
         if vin_code:
             print "vinCode: %s" % (vin_code)
-            result = do_task(vin_code)
-            if result is None:
+            results = do_task(vin_code)
+            if results is None:
                 # Requeue
                 mq.publish(vin_code)
             else:
-                insert_db(result, vin_code)
-            print "result: %s" % (result)
+                Mongo().insert_vin(results, vin_code)
+            print "final result: %s" % (results)
         else:
             print "no topic, to sleep 10 sec ..."
             time.sleep(10)
