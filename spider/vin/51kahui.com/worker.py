f5f0a8872f0f56bc062b97e06708b074cd965ddf
zhangzheng <zhangzheng@qianbao.com>
2016-12-25 18:14:26 +0800

    标准化 worker

--- spider/vin/51kahui.com/worker.py
+++ spider/vin/51kahui.com/worker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
 import requests
@@ -12,41 +12,51 @@ from pymongo import *
 ROOT_DIR = os.path.dirname(__file__)
 sys.path.append(ROOT_DIR)
 sys.path.append(os.path.join(ROOT_DIR, '../../../rabbitmq'))
+sys.path.append(os.path.join(ROOT_DIR, '../../../mongodb'))
 
 from rabbitmq import RabbitMQ
+from mongo import Mongo
 
 base_url = "http://spider.51kahui.com/CarInfo/vinQuery"
 timeout = 60
 
-client = MongoClient()
-client = MongoClient("localhost", 27017)
-db = client["vin"]
-collection = db["vin"]
-
 
 def do_task(vin_code):
+    """
+    Execute query VIN task
+
+    :vin_code: The 17 character VIN
+    :returns: list or None
+    """
     print "do_task(): %s" %(vin_code)
     payload = {"vinCode": vin_code}
-    response = requests.post(base_url, timeout=timeout, data=payload)
+    response = None
+    data = None
+    try:
+        response = requests.post(base_url, timeout=timeout, data=payload)
+    except Exception, msg:
+        print "Exception: ", msg
 
-    if response.status_code == 200:
+    if response is not None and response.status_code == 200:
         data = json.loads(response.text)
-        print data
+        # print data
         if data["status"] == "20000000":
             result = data["result"]
-            result["vinCode"] = vin_code[0:8]
-            result["vinLong"] = vin_code
-            collection.insert(result)
+            data = [result]
         else:
-            print "[1] %s not found" % (vin_code)
+            print "[1] %s not found, access restricted" % (vin_code)
     else:
-        print "[2] %s not found" % (vin_code)
+        print "[2] %s not found, network fault" % (vin_code)
+    return data
 
 
 def main():
+    db = Mongo()
     if len(sys.argv) == 2:
         vin_code = sys.argv[1]
-        do_task(vin_code)
+        data = do_task(vin_code)
+        if data:
+            db.insert_vin(data, vin_code)
         sys.exit(1)
     
     while True:
@@ -55,7 +65,11 @@ def main():
         if msg:
             print msg
             vin_code = msg
-            do_task(vin_code)
+            data = do_task(vin_code)
+            if data:
+                db.insert_vin(data, vin_code)
+            else:
+                mq.publish(vin_code)
         else:
             print "no topic, to sleep 10 sec ..."
             time.sleep(10)
