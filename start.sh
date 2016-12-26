65f407d3b8b7fd1de5478409d33a962f02b8467c
zhangzheng <zhangzheng@qianbao.com>
2016-12-26 09:30:48 +0800

    添加启动脚本

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+role=`id -u`
+if test $role -ne 0
+then
+    echo "运行脚本需要root权限"
+    exit 1
+fi
+
+mongodb/start.sh
+rabbitmq/start.sh
+
+sleep 5
+
+supervisor/install.sh
+supervisor/start.sh
+
