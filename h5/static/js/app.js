0e3461a45977fac22f574a085d17b9e9033bd80b
zhangzheng <zhangzheng@qianbao.com>
2016-12-26 11:25:32 +0000

    优化查询vin界面

--- h5/static/js/app.js
+++ h5/static/js/app.js
@@ -4,10 +4,10 @@ mainApp.controller("vinSearchController", function($scope, $http) {
     var baseUrl = "http://172.28.32.101:10090/vin/v1/";
     $scope.vinCode = "LVSHCAMB1CE054249";
     $scope.result = {};
-
-    $scope.search = function() {
+    $scope.search = function(vinCode) {
+        $scope.vinCode = vinCode;
         $http({
-            url: baseUrl + $scope.vinCode,
+            url: baseUrl + vinCode,
             method:'GET'
         }).success(function(data,header,config,status){
             $scope.result = data;
