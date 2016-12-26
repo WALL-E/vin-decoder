ab94a5035152734b0a0e52cb8d758486f31fa40e
zhangzheng <zhangzheng@qianbao.com>
2016-12-26 09:17:24 +0800

    优化代码 @rabbitmq.py

--- rabbitmq/rabbitmq.py
+++ rabbitmq/rabbitmq.py
@@ -3,50 +3,51 @@
 
 import pika
 
-class RabbitMQ():
+class RabbitMQ(object):
     def __init__(self, host="localhost", port=5672, queue="vin"):
         self.host = host
         self.port = port
         self.queue = queue
+        self.credentials = pika.PlainCredentials('guest', 'guest')
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.host, self.port, '/', self.credentials))
 
     def publish(self, content):
-        credentials = pika.PlainCredentials('guest', 'guest')
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(self.host, self.port, '/', credentials))
-        channel = connection.channel()
+        channel = self.connection.channel()
         channel.queue_declare(queue=self.queue)
         channel.basic_publish(exchange='', routing_key=self.queue, body=content)
-        connection.close()
+        channel.close()
 
     def start_consuming(self, callback):
-        credentials = pika.PlainCredentials('guest', 'guest')
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(self.host, self.port, '/', credentials))
-        channel = connection.channel()
+        channel = self.connection.channel()
         channel.queue_declare(queue=self.queue)
         channel.basic_consume(callback, queue=self.queue, no_ack=True)
         print(' [*] Waiting for messages. To exit press CTRL+C')
         channel.start_consuming()
 
     def basic_get(self):
-        credentials = pika.PlainCredentials('guest', 'guest')
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(self.host, self.port, '/', credentials))
-        channel = connection.channel()
+        channel = self.connection.channel()
+        result = None
         msg = channel.basic_get(queue=self.queue)
         if msg[0] is not None:
             channel.basic_ack(msg[0].delivery_tag)
             return msg[2]
-        return None
+        channel.close()
+        return result
 
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
 
-if __name__ == '__main__':
+def main():
     import time
     mq = RabbitMQ(queue="test")
     for i in range(5):
         mq.publish("hello, world, @%f" % (time.time()))
+    print "basic get:", mq.basic_get()
     mq.start_consuming(callback)
+
+
+if __name__ == '__main__':
+    main()
