59a6f5375ad47e7078d0b89c1922241c2128654e
zhangzheng <zhangzheng@qianbao.com>
2016-12-27 11:19:40 +0000

    添加worker_zz.py

---
+++ mongodb/worker_zz.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import requests
+import sys
+import json
+import os
+import time
+
+from pymongo import *
+
+ROOT_DIR = os.path.dirname(__file__)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, '../rabbitmq'))
+sys.path.append(os.path.join(ROOT_DIR, '../agent'))
+sys.path.append(os.path.join(ROOT_DIR, '../html'))
+
+from rabbitmq import RabbitMQ
+from robot import agent_vin144_net
+from parse import parse_html_vin114_net
+
+timeout = 60
+
+client = MongoClient()
+client = MongoClient("localhost", 27017)
+db = client["vin"]
+collection = db["vin"]
+
+
+def do_task(vin_code):
+    print "do_task(): %s" %(vin_code)
+    html = agent_vin144_net(vin_code)
+    if html is not None:
+        result = parse_html_vin114_net(html)  
+        data = json.loads(result)
+        data["vinCode"] = vin_code[0:8]
+        print data
+        collection.insert(data)
+
+
+def main():
+    if len(sys.argv) == 2:
+        vin_code = sys.argv[1]
+        do_task(vin_code)
+        sys.exit(1)
+    
+    while True:
+        mq = RabbitMQ(queue="vin")
+        msg = mq.basic_get()
+        if msg:
+            print msg
+            vin_code = msg
+            do_task(vin_code)
+        else:
+            print "no topic, to sleep 10 sec ..."
+            time.sleep(10)
+        time.sleep(30)
+
+if __name__ == '__main__':
+    main()
