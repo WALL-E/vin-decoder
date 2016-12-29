1834aa95b0538c152898a10f342169dd5fc7e8de
zhangzheng <zhangzheng@qianbao.com>
2016-12-29 09:20:08 +0000

    增加不使用代理的爬虫

---
+++ spider/vin/vincar.cn/robot_risk.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import sys
+import os
+import requests
+import json
+import time
+from bs4 import BeautifulSoup
+
+ROOT_DIR = os.path.dirname(__file__)
+sys.path.append(ROOT_DIR)
+
+def robot_html(vinCode):
+    home_url = "https://vincar.cn/"
+    post_url = "https://vincar.cn/vin"
+    timeout = 5
+    try_count = 10
+    html = None
+    result = None
+    headers_template = {
+        "Pragma": "no-cache",
+        "Origin": "https://vincar.cn",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Cache-Control": "no-cache",
+        "X-Requested-With": "XMLHttpRequest",
+        "Connection": "keep-alive",
+        "Referer": "https://vincar.cn/",
+    }
+
+    # 1-Request
+    response_1 = None
+    cookies = None
+    for i in range(try_count):
+        try:
+            response_1 = requests.get(home_url, timeout=timeout, verify=False)
+        except requests.exceptions.ConnectTimeout:
+            continue
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            continue
+
+        print "[1]", response_1
+        if response_1 is not None and response_1.status_code == 200:
+            cookies = response_1.cookies
+            break
+
+    if response_1 is None:
+        print "No agents available"
+        return html
+
+    soup = BeautifulSoup(response_1.text, "html.parser")
+    tokens = soup.findAll('meta', {"name": "csrf-token"})
+    token = tokens[0].attrs["content"]
+    headers_template["X-CSRF-Token"] = token
+
+    # 2-Request
+    payload = {'vin': vinCode}
+    response_2 = None
+    for i in range(try_count):
+        try:
+            response_2 = requests.post(post_url, timeout=timeout, data=payload, cookies=cookies, headers=headers_template, verify=False)
+        except requests.exceptions.ConnectTimeout:
+            continue
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            continue
+
+        print "[2]", response_2.text
+        if response_2.status_code == 200:
+            html = response_2.text
+            break
+
+    return html
+
+
+if __name__ == '__main__':
+    print robot_html("LVSHCAMB1CE054249")
---
+++ spider/vin/vincar.cn/worker_risk.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import requests
+import sys
+import json
+import os
+import time
+
+from pymongo import *
+
+ROOT_DIR = os.path.dirname(__file__)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, '../../../rabbitmq'))
+
+from rabbitmq import RabbitMQ
+from robot_risk import robot_html
+from parse import parse_html
+
+timeout = 60
+
+client = MongoClient()
+client = MongoClient("localhost", 27017)
+db = client["vin"]
+collection = db["vin"]
+
+
+def do_task(vin_code):
+    print "do_task(): %s" % (vin_code)
+    html = robot_html(vin_code)
+    if html is not None:
+        results = parse_html(html)  
+        return results
+    else:
+        print "[2] %s not found, download page failed" % (vin_code)
+    return None
+
+
+def insert_db(objs, vin_code):
+    for obj in objs:
+        obj["vinCode"] = vin_code[0:8]
+        obj["vinLong"] = vin_code
+        collection.insert(obj)
+
+def peek_task(vin_code):
+    objs = do_task(vin_code)
+    if objs:
+        insert_db(objs, vin_code)
+    print "result: %s" % (objs)
+
+def loop_task():
+    mq = RabbitMQ(queue="vin")
+    while True:
+        vin_code = mq.basic_get()
+        if vin_code:
+            print "vinCode: %s" % (vin_code)
+            results = do_task(vin_code)
+            if results:
+                insert_db(results, vin_code)
+            else:
+                # Requeue
+                mq.publish(vin_code)
+            print "result: %s" % (results)
+        else:
+            print "no topic, to sleep 10 sec ..."
+            time.sleep(10)
+        #time.sleep(5)
+
+def main():
+    if len(sys.argv) == 2:
+        vin_code = sys.argv[1]
+        peek_task(vin_code)
+    else:
+        loop_task()
+    
+if __name__ == '__main__':
+    main()
