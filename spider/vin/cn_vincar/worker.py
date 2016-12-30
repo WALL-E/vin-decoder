f407e6a5d73ad6d6cca1695b04da98980f38020a
zhangzheng <zhangzheng@qianbao.com>
2016-12-30 08:09:20 +0000

    规范worker.py

--- spider/vin/cn_vincar/worker.py
+++ spider/vin/cn_vincar/worker.py
@@ -1,54 +1,52 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+"""
+worker: query vin
+"""
 
-import requests
 import sys
 import json
 import os
 import time
 
-from pymongo import *
+import requests
 
 ROOT_DIR = os.path.dirname(__file__)
 sys.path.append(ROOT_DIR)
 sys.path.append(os.path.join(ROOT_DIR, '../../../rabbitmq'))
+sys.path.append(os.path.join(ROOT_DIR, '../../../mongodb'))
 
 from rabbitmq import RabbitMQ
+from mongo import Mongo
 from robot import robot_html
 from parse import parse_html
 
-timeout = 60
-
-client = MongoClient()
-client = MongoClient("localhost", 27017)
-db = client["vin"]
-collection = db["vin"]
-
 
 def do_task(vin_code):
+    """
+    Execute query VIN task
+
+    :vin_code: The 17 character VIN
+    :returns: list or None
+    """
     print "do_task(): %s" % (vin_code)
     html = robot_html(vin_code)
     if html is not None:
         results = parse_html(html)  
         return results
     else:
-        print "[2] %s not found, download page failed" % (vin_code)
+        print "%s not found, download page failed" % (vin_code)
     return None
 
 
-def insert_db(objs, vin_code):
-    for obj in objs:
-        obj["vinCode"] = vin_code[0:8]
-        obj["vinLong"] = vin_code
-        collection.insert(obj)
+def do_once(vin_code):
+    results = do_task(vin_code)
+    if results:
+        Mongo().insert_vin(results, vin_code)
+    print "result: %s" % (results)
 
-def peek_task(vin_code):
-    objs = do_task(vin_code)
-    if objs:
-        insert_db(objs, vin_code)
-    print "result: %s" % (objs)
 
-def loop_task():
+def do_loop():
     mq = RabbitMQ(queue="vin")
     while True:
         vin_code = mq.basic_get()
@@ -56,7 +54,7 @@ def loop_task():
             print "vinCode: %s" % (vin_code)
             results = do_task(vin_code)
             if results:
-                insert_db(results, vin_code)
+                Mongo().insert_vin(results, vin_code)
             else:
                 # Requeue
                 mq.publish(vin_code)
@@ -66,12 +64,13 @@ def loop_task():
             time.sleep(10)
         time.sleep(5)
 
+
 def main():
     if len(sys.argv) == 2:
         vin_code = sys.argv[1]
-        peek_task(vin_code)
+        do_once(vin_code)
     else:
-        loop_task()
+        do_loop()
     
 if __name__ == '__main__':
     main()
