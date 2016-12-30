edba790dc6a7c4c33cd148116492e65f72f0f98f
zhangzheng <zhangzheng@qianbao.com>
2016-12-30 05:48:23 +0000

    规范化parse.py

--- spider/vin/net_vin114/parse.py
+++ spider/vin/net_vin114/parse.py
@@ -1,47 +1,51 @@
 #!/usr/bin/python
 # coding:utf-8
-#
-# 从Html文件中解析数据
-#
-
+"""
+从Html文件中解析车辆信息
+"""
 
 import sys
 import os
-from bs4 import BeautifulSoup
 import json
+
+from bs4 import BeautifulSoup
 
 def parse_html(html):
     """
-    parse html of vin114.net
+    parse html of http://www.vin114.net/
 
     :param html: html strings
-    :returns: dict or None
+    :returns: list
     """
+    results = []
     try:
         soup = BeautifulSoup(html, "html.parser")
         tables = soup.findAll('table')
         table = tables[0]
 
-        result_list = []
+        tmp_list = []
         for row in table.findAll('tr'):
             for child in row.children:
                 ret = child.string.strip()
                 if len(ret) > 0:
-                    result_list.append(ret.encode("utf8"))
+                    tmp_list.append(ret.encode("utf8"))
 
-        result_dict = {}
-        for i in range(0, len(result_list), 2):
-            k = result_list[i]
-            v = result_list[i+1]
-            result_dict[k] = v
-
-        return result_dict
+        tmp_dict = {}
+        for i in range(0, len(tmp_list), 2):
+            key = tmp_list[i]
+            val = tmp_list[i+1]
+            tmp_dict[key] = val
+        results.append(tmp_dict)
     except Exception, msg:
         print "error: [parse html] %s" % (msg)
-        return None
+
+    return results
 
 
 def main():
+    """
+    main function
+    """
     if len(sys.argv) < 2:
         print "%s filename.html" % (sys.argv[0])
         sys.exit(1)
@@ -52,12 +56,8 @@ def main():
         sys.exit(1)
     html = open(filename)
     data = parse_html(html)
-    if data is None:
-        print "result:", data
-    else:
-        print "result:", json.dumps(data, encoding='UTF-8', ensure_ascii=False)
+    print "result:", json.dumps(data, encoding='UTF-8', ensure_ascii=False)
 
 
 if __name__ == "__main__":
     main()
-
