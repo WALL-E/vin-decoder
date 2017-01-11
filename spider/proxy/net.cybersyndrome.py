f7b3b16e4bf751d20a375d068c84474a52f642ed
zhangzheng <zhangzheng@qianbao.com>
2017-01-11 14:07:15 +0800

    增加代理爬虫

---
+++ spider/proxy/net.cybersyndrome.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2.7
+
+# -*- coding: UTF-8 -*-
+
+import sys
+import os
+import time
+import ipaddress
+from bs4 import BeautifulSoup
+
+ROOT_DIR = os.path.dirname(__file__)
+sys.path.append(ROOT_DIR)
+sys.path.append(os.path.join(ROOT_DIR, '../..'))
+
+from rabbitmq.rabbitmq import RabbitMQ
+from utils.downloader import get_page
+
+
+def parse_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    tables = soup.findAll('table')
+    table = tables[0]
+    mq = RabbitMQ(queue="cybersyndrome")
+    for tr in table.findAll('tr')[1:]:
+        tds = tr.findAll('td')
+        if len(tds) > 0:
+            content = tds[1].string
+            ip, port = content.split(":")
+            print content
+            try:
+                ip_tmp = ipaddress.IPv4Address(ip.decode("utf-8"))
+                if ip_tmp.is_private or ip_tmp.is_reserved or ip_tmp.is_multicast:
+                    print "private,reserved,multicast is not valid"
+                    continue
+            except ipaddress.AddressValueError, msg:
+                print msg
+                continue
+            mq.publish(content)
+
+
+def main():
+    home_url = "http://www.cybersyndrome.net/plr.html"
+
+    while True:
+        html = get_page(home_url, proxy_use=False)
+        parse_html(html)
+        time.sleep(300)
+
+if __name__ == '__main__':
+    main()
